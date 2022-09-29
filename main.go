3ef0a1ba382a3dd78a3640d7a72eebb26089eb9e
Khang Phan <khang.phan@opswat.com>
2022-09-30 01:23:55 +0700

    fix bug

--- main.go
+++ main.go
@@ -20,14 +20,38 @@ import (
 var client *mongo.Client
 
 func main() {
-	uri := getEnvVariable("URL_MONGODB")
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	check(err)
-	defer func() { // if main was exit this function was called to disconnect db
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Println("No .env file found")
+	// }
+	// uri := os.Getenv("URL_MONGODB")
+	// if uri == "" {
+	// 	log.Fatal("You must set your 'URL_MONGODB' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	// }
+	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer func() {
+	// 	if err := client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+	// coll := client.Database("sample_mflix").Collection("movies")
+	// title := "Back to the Future"
+	// var result bson.M
+	// err = coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
+	// if err == mongo.ErrNoDocuments {
+	// 	fmt.Printf("No document was found with the title %s\n", title)
+	// 	return
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// jsonData, err := json.MarshalIndent(result, "", "    ")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("%s\n", jsonData)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", getMovies).Methods("GET")
@@ -41,7 +65,7 @@ func main() {
 
 func check(err error) {
 	if err != nil {
-		fmt.Println("Error: ", err.Error())
+		panic(err)
 	}
 }
 
@@ -52,11 +76,20 @@ func getEnvVariable(key string) string {
 }
 
 func getMovie(w http.ResponseWriter, req *http.Request) {
+	uri := getEnvVariable("URL_MONGODB")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	check(err)
+	fmt.Printf("%+v\n", client)
+	defer func() { // if main was exit this function was called to disconnect db
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 	coll := client.Database("sample_mflix").Collection("movies")
 	title := "Back to the Future"
 
 	var result bson.M
-	err := coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
+	err = coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
 	if err == mongo.ErrNoDocuments {
 		fmt.Fprintf(w, "No document was found with the title %s\n", title)
 		return
@@ -73,6 +106,7 @@ func getMovie(w http.ResponseWriter, req *http.Request) {
 
 func getMovies(w http.ResponseWriter, req *http.Request) {
 
+	fmt.Fprintf(w, "%+v\n", client)
 }
 
 func postMovies(w http.ResponseWriter, req *http.Request) {
